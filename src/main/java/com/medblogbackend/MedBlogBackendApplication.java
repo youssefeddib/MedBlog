18977af1a8b63bb2d1d6ae970bf4d9ee7df6aa5e
youssef eddib <eddiby79@gmail.com>
2025-09-24 12:16:01 +0200

    architicture du projet

---
+++ src/main/java/com/medblogbackend/MedBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedBlogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedBlogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medblogbackend/MedBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedBlogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
